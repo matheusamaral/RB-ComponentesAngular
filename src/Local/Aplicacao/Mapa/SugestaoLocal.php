dfe00d70fe390e1c52da055af062c7b9b7a5479c
diego <diego@diego>
2017-01-06 18:25:31 -0200

    Merge origin/master
    Conflicts:
    	QuickPeek/www/js/Mapa/Estruturas/Mapa.css

--- src/Local/Aplicacao/Mapa/SugestaoLocal.php
+++ src/Local/Aplicacao/Mapa/SugestaoLocal.php
@@ -6,6 +6,18 @@ class SugestaoLocal {
     
     public function sugestaoLocal($msg){
         
-        
+        $usuarioId = $msg->getCampoSessao('dadosUsuarioLogado,id');
+        $query = Conteiner::get('ConsultaSugestaoLocal');
+        $checkIn = $query->consultarCheckIn($usuarioId); 
+        if($checkIn){
+            $msg->setResultadoEtapa(true, false, ['dados'=>$checkIn]);
+        }else{
+            $sugestao = $query->consultarSugestao($usuarioId);
+            if($sugestao){
+                $msg->setResultadoEtapa(true, false, ['dados'=>$sugestao]);
+            }else{
+                $msg->setResultadoEtapa(false);
+            }
+        }
     }
 }
