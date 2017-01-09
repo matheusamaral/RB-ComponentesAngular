8b30729fe855c5436a7171573d4d7b759d973a5f
diego <diego@diego>
2017-01-09 16:43:25 -0200

    Merge origin/master
    Conflicts:
    	QuickPeek/www/js/Mapa/Estruturas/Mapa.css

--- src/Local/Infra/Repositorio/Mapa/ConsultaSugestaoLocal.php
+++ src/Local/Infra/Repositorio/Mapa/ConsultaSugestaoLocal.php
@@ -9,10 +9,13 @@ class ConsultaSugestaoLocal {
         $query = Conteiner::get('Query', false);
         $query->select('l.id', 'localId')
                 ->add('l.titulo', 'localTitulo')
+                ->add('l.latitude', 'latitude')
+                ->add('l.longitude', 'longitude')
                 ->add('l.ativo', 'checkIn');
         $query->from('local', 'l');
         $query->join('check_in', 'c')
                 ->on('c.usuario_id = ?')
+                ->on('c.local_id = l.id')
                 ->on('c.presente = 1')
                 ->on('c.ativo = 1');
         $query->where('l.ativo = 1');
@@ -20,25 +23,27 @@ class ConsultaSugestaoLocal {
         return $query->executar('A');
     }
     
-    public function consultarSugestao($usuarioId){
+    public function consultarSugestao($usuarioId, $latitude, $longitude){
         
         $query = Conteiner::get('Query', false);
         $query->select('l.id', 'localId')
                 ->add('l.titulo', 'localTitulo')
-                ->add('    (6371 * ACOS(COS(RADIANS(- 16.7164795)) * COS(RADIANS(l.latitude)) * COS(RADIANS(- 42.8575164) '
-                        . '- RADIANS(l.longitude)) + SIN(RADIANS(- 16.7164795)) * SIN(RADIANS(l.latitude))))', 'distancia');
+                ->add('l.latitude', 'latitude')
+                ->add('l.longitude', 'longitude')
+                ->add('(6371 * ACOS(COS(RADIANS(?)) * COS(RADIANS(l.latitude)) * COS(RADIANS(?) '
+                        . '- RADIANS(l.longitude)) + SIN(RADIANS(?)) * SIN(RADIANS(l.latitude))))', 'distancia');
         $query->from('local', 'l');
         $query->join('check_in', 'c', 'left')
                 ->on('c.local_id = l.id')
                 ->on('c.usuario_id = ?')
                 ->on('c.presente = 0')
                 ->on('c.ativo = 1');
-        $query->where('(6371 * ACOS(COS(RADIANS(- 16.7164795)) * COS(RADIANS(l.latitude)) * COS(RADIANS(- 42.8575164) '
-                . '- RADIANS(l.longitude)) + SIN(RADIANS(- 16.7164795)) * SIN(RADIANS(l.latitude)))) <= 0.02');
+        $query->where('(6371 * ACOS(COS(RADIANS(?)) * COS(RADIANS(l.latitude)) * COS(RADIANS(?) '
+                . '- RADIANS(l.longitude)) + SIN(RADIANS(?)) * SIN(RADIANS(l.latitude)))) <= 0.02');
         $query->group('l.id');
-        $query->order('count(c.id) desc, distancia, c.id desc');
+        $query->order('count(c.id) desc, distancia, c.id desc, localId');
         $query->limit('1');
-        $query->addVariaveis($usuarioId);
+        $query->addVariaveis([$latitude, $longitude, $latitude, $usuarioId, $latitude, $longitude, $latitude]);
         return $query->executar('A');
     }
 }
