fd0ffa8173798bb86496feb0f832b15f6f684204
diego <diego@diego>
2017-01-09 18:14:48 -0200

    Merge origin/master
    Conflicts:
    	QuickPeek/www/js/Mapa/Estruturas/Mapa.css

--- src/Usuario/Aplicacao/Cadastro/SalvarFoto.php
+++ src/Usuario/Aplicacao/Cadastro/SalvarFoto.php
@@ -11,12 +11,10 @@ class SalvarFoto {
         $msg->setCampoSessao('ultimasImagens,0', DIR_BASE . $enderecoFoto);
         Conteiner::get('Base64')->upload($msg->getCampo('ArquivoBase64')->get('valor'), DIR_BASE.$enderecoFoto);
         $url = $this->imagemUpada('imagem', 'perfil', 0, 1);
-//        $caminho = [['url' => $url]];
-        $caminho = 'HTTP://192.168.0.121:8000/QuickPeek/quickpeek/' . $enderecoFoto;
+        $caminho = [['url' => $url]];
         
         if($url){
-            $msg->setCampoSessao('salvarFoto', $caminho);
-//            $msg->setCampoSessao('salvarFoto', $caminho[0]['url']);
+            $msg->setCampoSessao('salvarFoto', $caminho[0]['url']);
             $msg->setResultadoEtapa(true, false, ['endereco'=>$caminho[0]['url']]);
         }else{
             $msg->setResultadoEtapa(false);
