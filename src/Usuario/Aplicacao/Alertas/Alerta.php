5c95f7411a5718f9757e617b60bb1965a35cf467
diego <diego@diego>
2017-01-30 17:51:11 -0200

    Merge origin/master
    Conflicts:
    	QuickPeek/www/js/Mapa/Estruturas/Mapa.css

---
+++ src/Usuario/Aplicacao/Alertas/Alerta.php
@@ -0,0 +1,11 @@
+<?php
+namespace Quickpeek\Usuario\Aplicacao\Alertas;
+use Rubeus\ContenerDependencia\Conteiner;
+
+class Alerta {
+    
+    public function enviar($fields){
+        
+        
+    }
+}
