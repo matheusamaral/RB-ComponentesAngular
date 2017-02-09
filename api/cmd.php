2bf857cb7277644e7f61aacd850e2133c19de0bb
jose <jose@jcosta>
2017-02-09 12:34:00 -0200

    Merge origin/master
    Conflicts:
    	QuickPeek/www/js/LoadingInicial/Controladores/Acoes.js
    	api/iniciar.php

--- api/cmd.php
+++ api/cmd.php
@@ -72,9 +72,13 @@ class Chat implements MessageComponentInterface {
     }
     
     public function onMessage(ConnectionInterface $from, $mensagem){
-//        Sessao::setSessionPhp(1);
+        Sessao::limparSessao();
         $obj = json_decode($mensagem);
+        var_dump($obj->codsessrt,'=======');
         Sessao::iniciar($obj->codsessrt);
+        
+//        var_dump( Sessao::getSessao());
+        
         $msg = Conteiner::get('Mensagem', false);
         foreach($obj as $k=>$v){
             $msg->setCampo($k, $v);
